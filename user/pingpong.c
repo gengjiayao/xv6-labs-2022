38c9e613e0f09ae1f1c051eba36eb52c7979dbf8
gengjiayao <1098469310@qq.com>
2023-04-24 16:08:12 +0800

    Finish sleep and pingpong

---
+++ user/pingpong.c
@@ -0,0 +1,23 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main() {
+    int p[2];
+    pipe(p);    
+    if (fork() == 0) {
+        char bf[1];
+        read(p[0], bf, 1);
+        close(p[0]);
+        fprintf(1, "%d: received ping\n", getpid());
+        write(p[1], bf, 1);
+    } else {
+        char bf[1];
+        bf[0] = '1';
+        write(p[1], bf, 1);
+        close(p[1]);
+        read(p[0], bf, 1);
+        fprintf(1, "%d: received pong\n", getpid());
+    }
+    exit(0);
+}
